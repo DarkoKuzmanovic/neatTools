9bc2658c7f273d75d48a92a93829dbc2d196a83a
Darko Kuzmanovic <darko.kuzmanovic@gmail.com>
2025-01-25 15:24:41 +0100

    Vercel issue fix #2

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,8 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      runtime: process.env.VERCEL ? "nodejs20.x" : undefined,
+      runtime: "nodejs20.x",
+      regions: ["fra1"],
     }),
   },
 };
